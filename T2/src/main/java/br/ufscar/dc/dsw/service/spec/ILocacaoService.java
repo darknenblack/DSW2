845ecf6df55ed426dd50f348fa5d469d20fe2fb6
Sabrina Miranda <sbrina.smiranda@gmail.com>
2021-05-22 20:33:17 -0300

    T2

---
+++ T2/src/main/java/br/ufscar/dc/dsw/service/spec/ILocacaoService.java
@@ -0,0 +1,11 @@
+package br.ufscar.dc.dsw.service.spec;
+
+import java.util.List;
+import br.ufscar.dc.dsw.domain.Locacao;
+
+public interface ILocacaoService {
+	List<Locacao> buscarTodosSite(String site);
+	List<Locacao> buscarTodosLocadora(String locadora);
+	void salvar(Locacao locacao);
+	boolean verificaLocacao(String cliente, String locadora, String horario, String data);
+}

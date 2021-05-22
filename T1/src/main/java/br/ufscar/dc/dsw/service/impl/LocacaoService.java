243ff1e36182a4a64ee98bd33d00423d18f469c7
IsaacWillian <isaac.willian@gryfo.com.br>
2021-05-22 11:19:22 -0300

    Primeiro commit

---
+++ T1/src/main/java/br/ufscar/dc/dsw/service/impl/LocacaoService.java
@@ -0,0 +1,36 @@
+package br.ufscar.dc.dsw.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import br.ufscar.dc.dsw.dao.ILocacaoDAO;
+import br.ufscar.dc.dsw.domain.Locacao;
+import br.ufscar.dc.dsw.service.spec.ILocacaoService;
+
+@Service
+@Transactional(readOnly = false)
+public class LocacaoService implements ILocacaoService {
+
+	@Autowired
+	ILocacaoDAO dao;
+	
+	public void salvar(Locacao locacao) {
+		dao.save(locacao);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Locacao> buscarTodosCliente(String cliente) {
+		return dao.findAllByCliente(cliente);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Locacao> buscarTodosLocadora(String locadora) {
+		return dao.findAllByLocadora(locadora);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean verificaLocacao(String cliente, String locadora, String horario, String data) {
+		return !dao.verifyLocation(cliente, locadora, horario, data).isEmpty();
+	}
+}

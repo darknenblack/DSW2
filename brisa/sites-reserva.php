30396e1c95ca1605bc7ad01644e364863604fbe4
Sabrina Miranda <sbrina.smiranda@gmail.com>
2021-05-24 21:42:05 -0300

    Update sites-reserva.php

--- brisa/sites-reserva.php
+++ brisa/sites-reserva.php
@@ -5,7 +5,7 @@
 ?>
 <html>
 <head>
-<title>Brisa</title>
+<title>DiFerias</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -38,7 +38,6 @@ include ("menu.php");
       <!-- Repeat other header column ... -->
       </tr>
     </thead>
-    <tbody>
     <tr>
       <td class="gv-col">Booking</td>
       <td class="gv-col">https://booking.com</td>
@@ -53,6 +52,31 @@ include ("menu.php");
       <td class="gv-col">BestTrips</td>
       <td class="gv-col">https://best.trips</td>
       <td class="gv-col">admin@best.trips</td>
+    </tr>
+    <tr>
+      <td class="gv-col">Pousada Santa Rita</td>
+      <td class="gv-col">http://www.hotelpousadasantarita.com.br/</td>
+      <td class="gv-col">santarita@gmail.trips</td>
+    </tr>
+    <tr>
+      <td class="gv-col">Pousada Shangrila</td>
+      <td class="gv-col">https://www.shangrila.com.br/</td>
+      <td class="gv-col">Shangrila@gmail.com</td>
+    </tr>
+    <tr>
+      <td class="gv-col">Maresias Beira Mar</td>
+      <td class="gv-col">https://www.maresiasbeiramar.com.br/</td>
+      <td class="gv-col">beiraMaar@gmail.com.br</td>
+    </tr>
+    <tr>
+      <td class="gv-col">Pousada Caiçara</td>
+      <td class="gv-col">http://www.caicarapousada.com.br/</td>
+      <td class="gv-col">Caicara@bol.org</td>
+    </tr>
+    <tr>
+      <td class="gv-col">Pousada VivaMar</td>
+      <td class="gv-col">https://pousadavivamar.com.br/</td>
+      <td class="gv-col">VivaMar@gmail.org</td>
     </tr>
     </tbody>
   </table>

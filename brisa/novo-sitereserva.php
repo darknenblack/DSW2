c119cd498dd05c34ffb48521a7aca45f98ad1864
Sabrina Miranda <sbrina.smiranda@gmail.com>
2021-05-24 21:41:10 -0300

    Update novo-sitereserva.php

--- brisa/novo-sitereserva.php
+++ brisa/novo-sitereserva.php
@@ -5,7 +5,7 @@
 ?>
 <html>
 <head>
-<title>Brisa</title>
+<title>DiFerias</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -40,11 +40,16 @@
     <td><input type="password" name="senha"></td>
   </tr>
   <tr>
-    <td><input type="submit" value="Criar"></td>
+    <td><input type="submit" value="Criar" onclick="Sucesso()></td>
   </tr>
   </form>
   </table>
 </div>
 <?php include("footer.php"); ?>
+<script>
+  function Sucesso() {
+  alert("Site de reservas cadastrado com sucesso!");
+}
+</script>
 </body>
 </html>

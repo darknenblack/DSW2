4cd61001d1e3e94aa8449dea76fe89feb6f8f8a2
FernandoSNunes <fer_sassinunes@hotmail.com>
2021-06-07 13:07:29 -0300

    filtro por nome funcional

--- T2/static/Cartoes.js
+++ T2/static/Cartoes.js
@@ -1,14 +1,3 @@
-class promocao {
-    constructor(imgs, pousadas, sites, localizacoes, data_inicio, data_final) {
-      this.imgs = imgs;
-      this.pousadas = pousadas;
-      this.sites = sites;
-      this.localizacoes = localizacoes;
-      this.data_inicio = data_inicio;
-      this.data_final = data_final;
-    }
-}
-
 function myFunction() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("cidade");
@@ -18,35 +7,17 @@ function myFunction() {
     console.log(filter)
     console.log(date1)
     console.log(date2)
-
-    /*
-    if (filter === "aqui".toUpperCase()){
-        document.getElementById("data_inicio").style.display = "none";
-        console.log("entrou")
-    }else{
-        document.getElementById("data_inicio").style.display = "";
-
-        console.log("nao entrou")
-    }
-     */
-    cards = document.getElementsByClassName("cards")
-    nomes = document.getElementsByClassName("nomes")
-    for (i = 0; i <5; i++){
-        if (nomes[0].innerHTML.includes(filter))
-        card = cards[i]
-    }
-
-
-
-   /* for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+    
+    cards = document.getElementsByClassName("cards__item")
+    localizacoes = ["Ribeirão Preto", "Serra Negra", "Maresias", "Ilha Grande", "Ubatuba"]
+    //console.log(cards[0])
+    
+    for (i = 0; i < 5; i++){
+        if (localizacoes[i].toUpperCase().includes(filter)){
+          cards[i].style.display = ""
         }
-      }       
-    }*/
+        else{
+          cards[i].style.display = "none"
+        }
+    }
   }

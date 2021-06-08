ab7ee556bf93e33f903d5036e4054ad8de415330
FernandoSNunes <fer_sassinunes@hotmail.com>
2021-06-07 21:06:21 -0300

    Filtro do index funcional

--- T2/static/Cartoes.js
+++ T2/static/Cartoes.js
@@ -1,23 +1,48 @@
+function clearFilter(){
+  var input = document.getElementById("cidade");
+  var data1 = document.getElementById("data_inicio");
+  var data2 = document.getElementById("data_final");
+  input.value = ""
+  data1.value = ""
+  data2.value = ""
+  myFunction()
+}
+
+function getdate(date, part){
+  data = date.innerHTML.split("a")[part]
+  data= data.split("/")
+  d = new Date (data[2], data[1], data[0])
+  d.setMonth(d.getMonth() - 1);
+  return d
+}
+
 function myFunction() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("cidade");
-    filter = input.value.toUpperCase();
-    date1 = document.getElementById("data_inicio").value;
-    date2 = document.getElementById("data_final").value;
+    var input = document.getElementById("cidade");
+    const filter = input.value.toUpperCase();
+    var data1 = document.getElementById("data_inicio").value;
+    var data2 = document.getElementById("data_final").value;
     console.log(filter)
-    console.log(date1)
-    console.log(date2)
+    data1 = new Date (data1.replace("-", ","))
+    data2 = new Date (data2.replace("-", ","))
+    console.log(data1)
+    console.log(data2)
     
     cards = document.getElementsByClassName("cards__item")
     localizacoes = ["Ribeirão Preto", "Serra Negra", "Maresias", "Ilha Grande", "Ubatuba"]
+    date = document.getElementsByClassName("data")
     //console.log(cards[0])
     
-    for (i = 0; i < 5; i++){
-        if (localizacoes[i].toUpperCase().includes(filter)){
+    for (let i = 0; i < 5; i++){
+        if (localizacoes[i].toUpperCase().includes(filter) && ((isNaN(data1) || data1 >= getdate(date[i],0)) && (isNaN(data2) || data2 <= getdate(date[i],1)))){
           cards[i].style.display = ""
         }
         else{
           cards[i].style.display = "none"
         }
     }
+
+   
+   console.log(getdate(date[0], 1))
+   
+
   }

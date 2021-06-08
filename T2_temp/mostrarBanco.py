7b3f7c08ab95e7928c5c8ce30d2ace6c097123db
Sabrina Miranda <sbrina.smiranda@gmail.com>
2021-06-08 15:34:39 -0300

    excluindo temp e fazendo merge

--- T2_temp/mostrarBanco.py
+++
@@ -1,6 +0,0 @@
-from t2 import db,Hotel,User,Site,Promocao
-
-print(Site.query.all())
-print(User.query.all())
-print(Hotel.query.all())
-print(Promocao.query.all())
--- T2_temp/restartDb.py
+++
@@ -1,3 +0,0 @@
-from t2 import db
-db.drop_all()
-db.create_all()
